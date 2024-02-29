5583baca6d32d54dfffde88ed4933700567da436
TBSten <81161390+TBSten@users.noreply.github.com>
2024-03-01 06:59:59 +0900

    CameraPreviewにModifierを追加

--- camerax-compose/src/main/java/com/github/tbsten/cameraxcompose/CameraPreview.kt
+++ camerax-compose/src/main/java/com/github/tbsten/cameraxcompose/CameraPreview.kt
@@ -4,6 +4,7 @@ import android.view.ViewGroup
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
@@ -12,12 +13,13 @@ import androidx.lifecycle.LifecycleOwner
 @Composable
 fun CameraPreview(
     onBind: OnBindScope.() -> Unit,
+    modifier: Modifier = Modifier,
     onInitPreviewView: PreviewView.() -> Unit = {},
     lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
 ) {
     val context = LocalContext.current
-
     AndroidView(
+        modifier = modifier,
         factory = {
             PreviewView(it)
                 .apply {
