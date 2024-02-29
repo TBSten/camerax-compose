a8354acbc8af356334116ca5891ac6aa2eb57403
TBSten <81161390+TBSten@users.noreply.github.com>
2024-03-01 06:58:57 +0900

    CameraPreviewの初期化位置をAndroidViewのfactoryへ移動

--- camerax-compose/src/main/java/com/github/tbsten/cameraxcompose/CameraPreview.kt
+++ camerax-compose/src/main/java/com/github/tbsten/cameraxcompose/CameraPreview.kt
@@ -4,10 +4,6 @@ import android.view.ViewGroup
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
@@ -17,9 +13,6 @@ fun CameraPreview(
     onBind: OnBindScope.() -> Unit,
     onInitPreviewView: PreviewView.() -> Unit = {},
 ) {
-    var bindingState by remember {
-        mutableStateOf<BindingState>(BindingState.Initial)
-    }
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
 
@@ -34,32 +27,20 @@ fun CameraPreview(
                             ViewGroup.LayoutParams.MATCH_PARENT,
                         )
                 }.apply(onInitPreviewView)
-        },
-        update = { previewView ->
-            bindingState =
-                when (bindingState) {
-                    is BindingState.Initial ->
-                        try {
-                            val cameraProvider =
-                                ProcessCameraProvider
-                                    .getInstance(
-                                        context,
-                                    ).get()
-                            cameraProvider.unbindAll()
+                .also { previewView ->
+                    val cameraProvider =
+                        ProcessCameraProvider
+                            .getInstance(
+                                context,
+                            ).get()
+                    cameraProvider.unbindAll()
 
-                            OnBindScope(
-                                cameraProvider = cameraProvider,
-                                previewView = previewView,
-                                context = context,
-                                lifecycleOwner = lifecycleOwner,
-                            ).onBind()
-
-                            BindingState.Success
-                        } catch (e: Exception) {
-                            BindingState.Failed
-                        }
-
-                    is BindingState.Finish -> bindingState
+                    OnBindScope(
+                        cameraProvider = cameraProvider,
+                        previewView = previewView,
+                        context = context,
+                        lifecycleOwner = lifecycleOwner,
+                    ).onBind()
                 }
         },
     )
