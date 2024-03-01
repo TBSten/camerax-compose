815e50e59b1922c2a72f5b72d699d93b442892c5
TBSten <81161390+TBSten@users.noreply.github.com>
2024-03-02 06:34:46 +0900

    既存のAPIにKDocを追加

--- camerax-compose/src/main/java/com/github/tbsten/cameraxcompose/usecasehelper/ImageAnalysis.kt
+++ camerax-compose/src/main/java/com/github/tbsten/cameraxcompose/usecasehelper/ImageAnalysis.kt
@@ -3,6 +3,35 @@ package com.github.tbsten.cameraxcompose.usecasehelper
 import androidx.camera.core.ImageAnalysis
 import java.util.concurrent.Executor
 
+/**
+ * [ImageAnalysis]ユースケースに対応するUseCase helper。
+ *
+ * **使用例1** : 最小の例
+ * ```kotlin
+ * val imageAnalysis = imageAnalysisUseCase(executor) {
+ *   it.use { imageProxy ->
+ *     // TODO
+ *   }
+ * }
+ * ```
+ *
+ * **使用例2** : builderでオプションを指定
+ * ```kotlin
+ * val imageAnalysis = imageAnalysisUseCase(
+ *   executor = executor,
+ *   builder = { setTargetAspectRatio(AspectRatio.RATIO_4_3) },
+ * ) {
+ *   it.use { imageProxy ->
+ *     // TODO
+ *   }
+ * }
+ * ```
+ *
+ * @param executor [ImageAnalysis.setAnalyzer] に渡すexecutor。
+ * @param builder [ImageAnalysis.Builder] Builderでオプションを指定するためのコールバック。
+ * @param analyzer 画像のImageProxyを受け取って画像を処理するコールバック。ImageProxy.closeを呼び出さないと次のAnalyzerが起動しないので注意。
+ * @return [ImageAnalysis]ユースケースのインスタンス。
+ */
 fun imageAnalysisUseCase(
     executor: Executor,
     builder: ImageAnalysis.Builder.() -> ImageAnalysis.Builder = { this },
